344c66c1152f3d15449e4cdefb748eeef66670a9
Katherine <kfloresf1@miumg.edu.gt>
2023-10-22 21:13:57 -0600

    Creacion de Aplicacion Web de Gestion de Productos

---
+++ src/java/Clases/Producto.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author kathe
+ */
+public class Producto {
+    /*-------------------------------------- ATRIBUTOS DE CLASE -------------------------------------*/
+    
+    private int codigo;
+    private String nombre;
+    private int precio;
+    private int existencia;
+    private int marca;
+    private int categoria;
+    
+    /*----------------------------------- CONSTRUCTOR POR DEFECTO -----------------------------------*/
+    
+    public Producto(){
+    }
+    
+    /*---------------------------------- CONSTRUCTOR PERSONALIZADO ----------------------------------*/
+    
+    public Producto(int codigo, String nombre, int precio, int existencia, int marca, int categoria){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.existencia = existencia;
+        this.marca = marca;
+        this.categoria = categoria;
+    }
+
+    /*-------------------------------------- METODOS GET Y SET --------------------------------------*/
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+
+    public int getMarca() {
+        return marca;
+    }
+
+    public void setMarca(int marca) {
+        this.marca = marca;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+    
+}
